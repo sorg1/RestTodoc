1cb3217f55142cfabbcd7e57df603099973d5c22
sorg1 <sorg1@mail.ru>
2014-03-29 12:28:05 +0200

    Add SSTodoc/Global.asax.cs

---
+++ SSTodoc/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+using RestTodoc.ServiceInterface;
+using ServiceStack;
+
+namespace SSTodoc
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        public class AppHost : AppHostBase
+        {
+            public AppHost() : base("Todoc API", typeof(TodocService).Assembly) { }
+
+
+            public override void Configure(Funq.Container container)
+            {
+                //Custom global uncaught exception handling strategy
+                this.UncaughtExceptionHandlers.Add((req, res, operationName, ex) =>
+                {
+                    res.StatusCode = 400;
+                    res.Write(string.Format("Exception {0}", ex.GetType().Name));
+                    res.EndRequest(skipHeaders: true);
+                });
+            }
+        }
+
+        protected void Application_Start(Object sender, EventArgs e)
+        {
+            (new AppHost()).Init();
+        }
+    }
+}
